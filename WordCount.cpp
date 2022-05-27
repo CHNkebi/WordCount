ee96d63ed71e0620710345ccfe0303567c140148
zxy <404837319@qq.com>
2022-05-27 18:44:28 +0800

    增加了文件操作

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,17 +1,32 @@
 #include<stdio.h>
 #include<string.h>
 
-#define MAX 1000
+#define MAXTITLE 100
+#define MAXTEXT 1000
+
 
 int main() {
-	char s[MAX];
-	gets(s);
+	
+	char con[3];
+	char fileName[MAXTITLE];
+	
+	scanf(" %s", con);
+	scanf(" %s", fileName);
+	
+	FILE* pf = fopen(fileName, "r");
+	if(pf == NULL) {
+		printf("文件名输入有误");
+		return 0; 
+	}
+	
+	char s[MAXTEXT];
+	fread(s, 1, MAXTEXT, pf);
+	
 	int n = strlen(s);
-	int charNum = 0, wordNum = 0;
+	int wordNum = 0;
 	bool flag = false;
 	for(int i = 0; i < n; i++) {
 		if(s[i] != ' ' && s[i] != ',') {
-			charNum++;
 			flag = true;
 		} else if(flag) {
 			wordNum++;
@@ -19,5 +34,10 @@ int main() {
 		}
 	}
 	if(s[n - 1] != ' ' && s[n - 1] != ',') wordNum++;
-	printf("字符数: %d\n单词数: %d", charNum, wordNum);
+	
+	if(stricmp(con,"-c") == 0)
+		printf("%s中的字符数为：%d", fileName, n);
+	else if(stricmp(con, "-w") == 0)
+		printf("%s中的单词数为：%d", fileName, wordNum);
+	else printf("控制参数输入有误");
 }
